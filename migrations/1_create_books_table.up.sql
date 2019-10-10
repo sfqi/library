21e4f4fc5b7e601eec4a63dbbf220c094cc11aeb
bojan <boki1997@gmail.com>
2019-10-10 13:21:11 +0200

    Table scheme modified

--- migrations/1_create_books_table.up.sql
+++ migrations/1_create_books_table.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE books (
-    title varchar(50),
-    author_id varchar(50),
-    isbn varchar(50) unique,
-    isbn_13 varchar(50),
+    id integer not null AUTO_INCREMENT primary key,
+    title varchar(255) not null,
+    author varchar(255),
+    isbn varchar(10) unique not null,
+    isbn_13 varchar(13) not null,
     open_library_id varchar(50),
     cover_id varchar(50),
     year integer ,
-    publsher varchar(50)
+    publisher varchar(255)
 )
