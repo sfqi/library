7624d3d34925149461b43dfd4853cd03c9d49bc2
StefanGajic <stefan.gajic@ymail.com>
2019-11-15 12:07:41 +0100

    Added update book method with gorm

--- repository/postgres/postgres.go
+++ repository/postgres/postgres.go
@@ -16,36 +16,39 @@ type PostgresConfig struct {
 	Name     string
 }
 
-
-
 type Store struct {
 	db *gorm.DB
 }
 
-func NewStore(db *gorm.DB)*Store{
+func NewStore(db *gorm.DB) *Store {
 	return &Store{db}
 }
 
-func Open(config PostgresConfig)(*Store, error){
+func Open(config PostgresConfig) (*Store, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		config.Host,config.Port,config.User,config.Password,config.Name)
+		config.Host, config.Port, config.User, config.Password, config.Name)
 
-	db, err := gorm.Open("postgres",psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	store := NewStore(db)
-	return store,nil
+	return store, nil
 }
-func(store *Store)FindById(id int)(*model.Book, error){
+func (store *Store) FindById(id int) (*model.Book, error) {
 	b := model.Book{}
-	if err := store.db.First(&b,id).Error;err!=nil{
-		return nil,err
+	if err := store.db.First(&b, id).Error; err != nil {
+		return nil, err
 	}
-	return &b,nil
+	return &b, nil
 }
 
-func (store *Store)CreateBook(book *model.Book)(err error){
-	return store.db.Create(&book).Error //Save will insert primary key if it doesn't exis
+func (store *Store) CreateBook(book *model.Book) (err error) {
+	return store.db.Create(&book).Error
+
+}
+
+func (store *Store) UpdateBook(book model.Book) (err error) {
+	return store.db.Save(&book).Error
 }
