c7546ce44902e1de4587481f5814356997fab7ba
bojan <boki1997@gmail.com>
2019-11-18 12:01:05 +0100

    Fixed taggs for gorm

--- domain/model/book.go
+++ domain/model/book.go
@@ -6,11 +6,11 @@ type Book struct {
 	Id            int
 	Title         string
 	Author        string
-	Isbn          string    `gorm:"colum:isbn_10"`
-	Isbn13        string    `gorm:"column:isbn_13"`
-	OpenLibraryId string    `gorm:"column:open_library_id"`
-	CoverId       string    `gorm:"column:cover_id"`
-	Year          string    `gorm:"column:publish_date"`
-	CreatedAt     time.Time `gorm:"column:created_at"`
-	UpdatedAt     time.Time `gorm:"column:updated_at"`
+	Isbn          string
+	Isbn13        string
+	OpenLibraryId string
+	CoverId       string
+	Year          string    `gorm:"-"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 }
