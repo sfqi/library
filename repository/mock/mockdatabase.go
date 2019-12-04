1985d45c75f740553a0786092fb737d69e0211f3
bojan <boki1997@gmail.com>
2019-12-04 11:22:40 +0100

    Deleted filed book from structure

--- repository/mock/mockdatabase.go
+++ repository/mock/mockdatabase.go
@@ -1,15 +1,32 @@
 package mock
 
-import "github.com/sfqi/library/domain/model"
+import (
+	"github.com/sfqi/library/domain/model"
+)
 
 type Store struct{
-	Book *model.Book
 	Books []*model.Book
 	Err error
 }
 
+func NewStore(books []*model.Book,err error)*Store{
+	return &Store{
+		Books: books,
+		Err:err,
+	}
+}
+
 func (s *Store)FindBookById(id int) (*model.Book, error){
-	return s.Book,s.Err
+	return s.findBookByID(id),s.Err
+}
+
+func (s *Store) findBookByID(id int) (*model.Book) {
+	for _, b := range s.Books {
+		if b.Id == id {
+			return b
+		}
+	}
+	return nil
 }
 
 func (s *Store)CreateBook(book *model.Book) error{
