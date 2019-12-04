542446b8cc47cc24211737fedad2b4c250c32905
bojan <boki1997@gmail.com>
2019-12-04 11:34:21 +0100

    One structure for both models

--- repository/inmemory/userDB.go
+++ repository/inmemory/userDB.go
@@ -6,44 +6,13 @@ import (
 	"time"
 )
 
-var users = []model.User{
-	{
-		Id:        1,
-		Email:     "prvi@email.com",
-		Name:      "ime",
-		LastName:  "prezime",
-		CreatedAt: time.Now().Add(-10 * time.Second),
-		UpdatedAt: time.Now().Add(-8 * time.Second),
-	},
-	{
-		Id:        2,
-		Email:     "drugi@email.com",
-		Name:      "ime2",
-		LastName:  "prezime2",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	},
+func (db *DB) FindUserById(id int) (*model.User, error) {
+	user, _, err := db.findUserById(id)
+	return user, err
 }
 
-type UserDb struct{
-	Id int
-	users []model.User
-}
-
-func NewUserDb()*UserDb{
-	return &UserDb{
-		Id:    len(users),
-		users: users,
-	}
-}
-
-func(udb *UserDb) FindUserById(id int)(*model.User,error){
-	user,_,err := udb.findUserById(id);
-	return user,err
-}
-
-func (udb *UserDb) findUserById(id int)(*model.User,int,error){
-	for i, u := range udb.users {
+func (db *DB) findUserById(id int) (*model.User, int, error) {
+	for i, u := range db.users {
 		if u.Id == id {
 			return &u, i, nil
 		}
@@ -51,30 +20,28 @@ func (udb *UserDb) findUserById(id int)(*model.User,int,error){
 	return nil, -1, fmt.Errorf("error while finding User")
 }
 
-func(udb *UserDb) FindAllUsers()([]*model.User,error){
-	pointers := make([]*model.User,len(udb.users))
-	for i:=0;i<len(udb.users);i++{
-		pointers[i] = &udb.users[i]
+func (db *DB) FindAllUsers() ([]*model.User, error) {
+	pointers := make([]*model.User, len(db.users))
+	for i := 0; i < len(db.users); i++ {
+		pointers[i] = &db.users[i]
 	}
 	fmt.Println(pointers)
-	return pointers,nil
+	return pointers, nil
 }
 
-
-func (udb *UserDb) CreateUser(user *model.User) error {
-	udb.Id++
+func (db *DB) CreateUser(user *model.User) error {
+	db.Id++
 	now := time.Now()
 	user.CreatedAt = now
 	user.UpdatedAt = now
 
-	user.Id = udb.Id
-	udb.users = append(udb.users, *user)
+	user.Id = db.Id
+	db.users = append(db.users, *user)
 	return nil
 }
 
-
-func (udb *UserDb) UpdateUser(toUpdate *model.User) error {
-	user, index, err := udb.findUserById(toUpdate.Id)
+func (db *DB) UpdateUser(toUpdate *model.User) error {
+	user, index, err := db.findUserById(toUpdate.Id)
 	if err != nil {
 		return err
 	}
@@ -82,15 +49,15 @@ func (udb *UserDb) UpdateUser(toUpdate *model.User) error {
 	user.Name = toUpdate.Name
 	user.LastName = toUpdate.LastName
 	toUpdate = user
-	udb.users[index] = *user
+	db.users[index] = *user
 	return nil
 }
 
-func (udb *UserDb) DeleteUser(user *model.User) error {
-	_, loc, err := udb.findUserById(user.Id)
+func (db *DB) DeleteUser(user *model.User) error {
+	_, loc, err := db.findUserById(user.Id)
 	if err != nil {
 		return err
 	}
-	udb.users = append(udb.users[:loc], udb.users[loc+1:]...)
+	db.users = append(db.users[:loc], db.users[loc+1:]...)
 	return nil
 }
