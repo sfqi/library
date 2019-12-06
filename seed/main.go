997c4a338458212fb9f63e0e94fbf4e99e11f3a0
sonja <stefan.gajic@ymail.com>
2019-12-06 13:37:03 +0100

    Changed the TODO comment to English and fixed the indentation

--- seed/main.go
+++ seed/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"github.com/sfqi/library/domain/model"
-	"fmt"
 	"time"
+
+	"github.com/sfqi/library/domain/model"
 	"github.com/sfqi/library/repository/postgres"
 )
 
-	var books = []*model.Book{
-		{
-			Title:         "Information systems literacy.",
-			Author:        "Hossein Bidgoli",
-			Isbn:          "0023095334",
-			Isbn13:        "9780023095337",
-			OpenLibraryId: "OL1733511M",
-			Year:          1993,
-			Publisher: 	   "Maxwell Macmillan International",
-			CreatedAt:     time.Now(),
-			UpdatedAt:     time.Now(),
-		},
-		{
-			Title:         "Programming the World Wide Web",
-			Author:        "Robert W. Sebesta",
-			Isbn:          "0321303326",
-			Isbn13:        "9780321303325",
-			OpenLibraryId: "OL3393672M",
-			Year:          2005,
-			Publisher: 	   "Pearson/Addison-Wesley",
-			CreatedAt:     time.Now(),
-			UpdatedAt:     time.Now(),
-		},
-	}
+var books = []*model.Book{
+	{
+		Title:         "Information systems literacy.",
+		Author:        "Hossein Bidgoli",
+		Isbn:          "0023095334",
+		Isbn13:        "9780023095337",
+		OpenLibraryId: "OL1733511M",
+		Year:          1993,
+		Publisher:     "Maxwell Macmillan International",
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+	},
+	{
+		Title:         "Programming the World Wide Web",
+		Author:        "Robert W. Sebesta",
+		Isbn:          "0321303326",
+		Isbn13:        "9780321303325",
+		OpenLibraryId: "OL3393672M",
+		Year:          2005,
+		Publisher:     "Pearson/Addison-Wesley",
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+	},
+}
 
 func main() {
 
@@ -43,10 +43,9 @@ func main() {
 	}
 
 	store, err := postgres.Open(config)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Successfully connected")
 
 	for _, book := range books {
 		if err := store.CreateBook(book); err != nil {
