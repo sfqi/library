01de5862dfccf7bc938946e5053c12eb6af00c97
bojan <boki1997@gmail.com>
2019-12-11 13:27:31 +0100

    Mocking client using testify.Mock

--- openlibrary_test/client_test.go
+++ openlibrary_test/client_test.go
@@ -1,37 +1,50 @@
 package openlibrary_test
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
-	"github.com/sfqi/library/openlibrary"
-
+	"github.com/stretchr/testify/mock"
 	"github.com/sfqi/library/openlibrary/dto"
 )
 
+type ClientMock struct{
+	mock.Mock
+	Book *dto.Book
+	Err  error
+}
+
+func(cm *ClientMock)FetchBook(isbn string) (*dto.Book, error){
+	fmt.Println("Input isbn is: " + isbn)
+	args := cm.Called(isbn)
+	err := args.Error(1)
+	if err != nil{
+		return nil, err
+	}
+	// Cim imamo error, znamo da je knjiga nil, sa ovim if-om izbegavamo
+	// da program pukne zbog konverzije  -- Izbrisacu
+	book := args.Get(0).(dto.Book)
+	return &book,args.Error(1)
+}
+
 func TestFetchBook(t *testing.T) {
 	t.Run("book is successfully fetched", func(t *testing.T) {
-		expected := `{ISBN:0201558025": {"title": "Concrete mathematics"}}`
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte(`{"ISBN:0201558025": {"title": "Concrete mathematics"}}`))
 		}))
-
 		defer server.Close()
 
-		client := openlibrary.NewClient(server.URL)
-
-		responseBook, err := client.FetchBook("0201558025")
-
-		if err != nil {
-			t.Errorf("Got error: %s", err)
-		}
-
-		if responseBook.Title != "Concrete mathematics" {
-			t.Errorf("We did not get the expected response,expected %s, got %s", expected, responseBook.Title)
-		}
+		mockClient := ClientMock{}
+		mockClient.On("FetchBook","0201558025").Return(dto.Book{
+			Title:      "Concrete mathematics",
+			Year:       2019,
+		},nil)
+		mockClient.FetchBook("0201558025")
+		mockClient.AssertExpectations(t)
 	})
 
 	t.Run("error decoding from fetchBook", func(t *testing.T) {
@@ -42,13 +55,10 @@ func TestFetchBook(t *testing.T) {
 
 		defer server.Close()
 
-		client := openlibrary.NewClient(server.URL)
-		responseBook, err := client.FetchBook("0140447938")
-
-		checkError(t, err, "error while decoding from FetchBook:")
-
-		checkIfBookIsNil(responseBook, t)
-
+		mockClient := ClientMock{}
+		mockClient.On("FetchBook","aaa0201558025").Return(nil,errors.New("error while decoding from FetchBook:"))
+		mockClient.FetchBook("aaa0201558025")
+		mockClient.AssertExpectations(t)
 	})
 
 	t.Run("server response doesn't contain the expected ISBN key", func(t *testing.T) {
@@ -58,26 +68,10 @@ func TestFetchBook(t *testing.T) {
 		}))
 		defer server.Close()
 
-		client := openlibrary.NewClient(server.URL)
-		responseBook, err := client.FetchBook("0140447932111xxxx")
-
-		checkError(t, err, "value for given key cannot be found:")
-
-		checkIfBookIsNil(responseBook, t)
+		mockClient := ClientMock{}
+		mockClient.On("FetchBook","02015580251111").Return(nil,errors.New("value for given key cannot be found:"))
+		mockClient.FetchBook("02015580251111")
+		mockClient.AssertExpectations(t)
 	})
 }
 
-func checkError(t *testing.T, err error, expected string) {
-	if err == nil {
-		t.Error("Expected error ,  got nil")
-	}
-	if !strings.Contains(err.Error(), expected) {
-		t.Errorf("Got error: %s", err)
-	}
-}
-
-func checkIfBookIsNil(b *dto.Book, t *testing.T) {
-	if b != nil {
-		t.Errorf("Expected Book to be nil, got : %v", b)
-	}
-}
