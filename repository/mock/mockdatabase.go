e2c3c5e1b5d385e39bb90cb7ce1bcac24768b324
bojan <boki1997@gmail.com>
2019-12-13 12:03:31 +0100

    Mock structure in repository/mock

--- repository/mock/mockdatabase.go
+++ repository/mock/mockdatabase.go
@@ -2,45 +2,57 @@ package mock
 
 import (
 	"github.com/sfqi/library/domain/model"
+	"github.com/stretchr/testify/mock"
 )
 
-type Store struct{
-	Books []*model.Book
-	Err error
+type StoreMock struct {
+	mock.Mock
 }
 
-func NewStore(books []*model.Book,err error)*Store{
-	return &Store{
-		Books: books,
-		Err:err,
+func (sm *StoreMock) FindBookById(id int) (*model.Book, error) {
+	args := sm.Called(id)
+	if args.Get(0) != nil {
+		return args.Get(0).(*model.Book), nil
 	}
+	return nil, args.Error(1)
 }
 
-func (s *Store)FindBookById(id int) (*model.Book, error){
-	return s.findBookByID(id),s.Err
-}
-
-func (s *Store) findBookByID(id int) (*model.Book) {
-	for _, b := range s.Books {
-		if b.Id == id {
-			return b
-		}
+func (sm *StoreMock) findBookByID(id int) *model.Book {
+	args := sm.Called(id)
+	if args.Get(0) != nil {
+		return args.Get(0).(*model.Book)
 	}
 	return nil
 }
 
-func (s *Store)CreateBook(book *model.Book) error{
-	return s.Err
+func (sm *StoreMock) CreateBook(book *model.Book) error {
+	args := sm.Called(book)
+	if args.Get(0) != nil {
+		return args.Error(0)
+	}
+	return nil
 }
 
-func (s *Store)UpdateBook(book *model.Book) error{
-	return s.Err
+func (sm *StoreMock) UpdateBook(book *model.Book) error {
+	args := sm.Called(book)
+	if args.Get(0) != nil {
+		return args.Error(0)
+	}
+	return nil
 }
 
-func (s *Store)FindAllBooks() ([]*model.Book, error){
-	return s.Books, s.Err
+func (sm *StoreMock) FindAllBooks() ([]*model.Book, error) {
+	args := sm.Called()
+	if args.Get(0) != nil {
+		return args.Get(0).([]*model.Book), nil
+	}
+	return nil, args.Error(1)
 }
 
-func(s *Store)DeleteBook(book *model.Book)error{
-	return s.Err
+func (sm *StoreMock) DeleteBook(book *model.Book) error {
+	args := sm.Called(book)
+	if args.Get(0) != nil {
+		return args.Error(0)
+	}
+	return nil
 }
