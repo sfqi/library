3bd47d25622ec168e3c7307636d291da71c17070
sonja <stefan.gajic@ymail.com>
2019-12-25 12:03:24 +0100

    Added Delete for interactor and TestDelete also

--- interactor/book_test.go
+++ interactor/book_test.go
@@ -1,6 +1,14 @@
 package interactor_test
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"github.com/sfqi/library/domain/model"
+	"github.com/sfqi/library/interactor"
+	"github.com/sfqi/library/repository/mock"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBook_Create(t *testing.T) {
 
@@ -14,8 +22,34 @@ func TestBook_FindById(t *testing.T) {
 
 }
 
-func TestBook_Delete(t *testing.T) {
+func TestDelete(t *testing.T) {
+	t.Run("Book successfully deleted", func(t *testing.T) {
+		var db = &mock.Store{}
 
+		b := interactor.NewBook(db, nil)
+
+		bookToDelete := &model.Book{}
+
+		db.On("DeleteBook", bookToDelete).Return(nil)
+
+		err := b.Delete(bookToDelete)
+
+		assert.NoError(t, err)
+
+	})
+	t.Run("Error deleting book", func(t *testing.T) {
+		var db = &mock.Store{}
+
+		b := interactor.NewBook(db, nil)
+
+		bookToDelete := &model.Book{}
+
+		db.On("DeleteBook", bookToDelete).Return(errors.New("Error while deleting book"))
+
+		err := b.Delete(bookToDelete)
+
+		assert.Error(t, err)
+	})
 }
 
 func TestBook_Update(t *testing.T) {
