e11be047935196f09976c7c35de2ac1870628c7d
sonja <stefan.gajic@ymail.com>
2019-12-25 12:47:10 +0100

    Added assert.New(), checked exact value, and checked error

--- interactor/book_test.go
+++ interactor/book_test.go
@@ -15,12 +15,13 @@ func TestBook_Create(t *testing.T) {
 }
 
 func TestFindAll(t *testing.T) {
+	assert := assert.New(t)
 	t.Run("Successfully returned books", func(t *testing.T) {
-		var db = &mock.Store{}
+		store := &mock.Store{}
 
-		b := interactor.NewBook(db, nil)
+		b := interactor.NewBook(store, nil)
 
-		db.On("FindAllBooks").Return([]*model.Book{
+		store.On("FindAllBooks").Return([]*model.Book{
 			{
 				Id:            1,
 				Title:         "some title",
@@ -42,21 +43,46 @@ func TestFindAll(t *testing.T) {
 				Year:          2019,
 			}}, nil)
 
+		expectedBooks := []*model.Book{
+			{
+				Id:            1,
+				Title:         "some title",
+				Author:        "some author",
+				Isbn:          "some isbn",
+				Isbn13:        "some isbon13",
+				OpenLibraryId: "again some id",
+				CoverId:       "some cover ID",
+				Year:          2019,
+			},
+			{
+				Id:            2,
+				Title:         "other title",
+				Author:        "other author",
+				Isbn:          "other isbn",
+				Isbn13:        "other isbon13",
+				OpenLibraryId: "other some id",
+				CoverId:       "other cover ID",
+				Year:          2019,
+			},
+		}
+
 		books, err := b.FindAll()
-		assert.NotNil(t, books)
-		assert.NoError(t, err)
+		assert.Equal(books, expectedBooks)
+		assert.NoError(err)
 	})
 	t.Run("Error retrieving books", func(t *testing.T) {
-		var db = &mock.Store{}
+		store := &mock.Store{}
+		storeError := errors.New("Error finding books")
 
-		b := interactor.NewBook(db, nil)
+		b := interactor.NewBook(store, nil)
 
-		db.On("FindAllBooks").Return(nil, errors.New("Error finding books"))
+		store.On("FindAllBooks").Return(nil, errors.New("Error finding books"))
 
 		book, err := b.FindAll()
 
-		assert.Nil(t, book)
-		assert.Error(t, err)
+		assert.Nil(book)
+		assert.Error(err)
+		assert.Equal(errors.New("Error finding books"), storeError)
 	})
 
 }
