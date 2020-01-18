26236b8413fa31993d478dd29ec20694e314c73c
sonja <stefan.gajic@ymail.com>
2020-01-18 22:47:11 +0100

    Added error for unknown error type, return empty json array if loans is 0 and fixed some errors

--- handler/loanhandler.go
+++ handler/loanhandler.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 	"strconv"
 
@@ -23,10 +22,9 @@ type loanInteractor interface {
 }
 
 func (l *LoanHandler) FindLoansByUserID(w http.ResponseWriter, r *http.Request) *HTTPError {
-
 	id, err := strconv.Atoi(mux.Vars(r)["user_id"])
 	if err != nil {
-		return newHTTPError(http.StatusBadRequest, err)
+		return newHTTPError(http.StatusNotFound, err)
 	}
 
 	loans, err := l.Interactor.FindByUserID(id)
@@ -34,21 +32,36 @@ func (l *LoanHandler) FindLoansByUserID(w http.ResponseWriter, r *http.Request) 
 		return newHTTPError(http.StatusInternalServerError, err)
 	}
 	if len(loans) == 0 {
-		return newHTTPError(http.StatusNotFound, errors.New("No loans for given user id can be found"))
+		json.NewEncoder(w).Encode(loans)
 	}
 
 	var loanResponses []*dto.LoanResponse
+
 	for _, loan := range loans {
-		loanResponses = append(loanResponses, toLoanResponse(*loan))
+		loans, err := toLoanResponse(loan)
+		if err != nil {
+			return newHTTPError(http.StatusNotFound, err)
+		}
+		loanResponses = append(loanResponses, loans)
 	}
 
 	err = json.NewEncoder(w).Encode(loanResponses)
 	if err != nil {
-		return newHTTPError(http.StatusBadRequest, err)
+		return newHTTPError(http.StatusInternalServerError, err)
 	}
 	return nil
 }
 
-func toLoanResponse(b model.Loan) *dto.LoanResponse {
-	return dto.CreateLoanResponse(b.ID, b.TransactionID, b.UserID, b.BookID, b.PrintType())
+func toLoanResponse(l *model.Loan) (*dto.LoanResponse, *HTTPError) {
+	loanType, err := l.PrintType()
+	if err != nil {
+		return nil, newHTTPError(http.StatusNotFound, err)
+	}
+
+	loanResponse, err := dto.CreateLoanResponse(l.ID, l.TransactionID, l.UserID, l.BookID, loanType)
+	if err != nil {
+		return nil, newHTTPError(http.StatusNotFound, err)
+	}
+
+	return loanResponse, nil
 }
